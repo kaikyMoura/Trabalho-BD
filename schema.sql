7cccfdf783032b79721f0c8f485e3d8aa85fbd5c
kaiky de Moura <kaikymoura972@gmail.com>
2022-05-27 15:40:31 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE if not exists Channell (
     channellid VARCHAR(24) NOT NULL,
     channelTitle VARCHAR(120) NOT NULL, 
     description_ VARCHAR(500),
+    content_id varchar(24) NOT NULL,
     customUrl VARCHAR(255), 
     publishedAt VARCHAR(255) NOT NULL,
     PRIMARY KEY (channellid), 
@@ -20,6 +21,7 @@ CREATE TABLE if not exists Channell (
 
 CREATE TABLE if not exists video (  
     videoId VARCHAR(14) NOT NULL,
+    channellid VARCHAR(24) NOT NULL,
     link VARCHAR(255),
     title VARCHAR(255),
     publishedAt DATETIME, 
@@ -29,22 +31,20 @@ CREATE TABLE if not exists video (
 );
 
 CREATE TABLE if not exists video_statistics (
-    id int(11) NOT NULL,
     viewCount int(255), 
     likeCount int(255),
     dislikeCount int(255),  
     favoriteCount int(255), 
     commentCount int(255),
-    PRIMARY KEY(id),
+    videoId VARCHAR(14) NOT NULL,
     FOREIGN KEY(videoId) references video(videoId)
 );
 
 CREATE TABLE if not exists channel_statistics (
-    id int(11) NOT NULL,
     viewCount int(255),
+    channellid VARCHAR(24) NOT NULL,
     subscriberCount int(255),
     hiddenSubscriberCount int(255),
     videoCount int(255),
-    PRIMARY KEY(id),
     FOREIGN KEY(channellid) references channell(channellid)
 );
