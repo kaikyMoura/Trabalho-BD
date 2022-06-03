de6aff69430d75c9894e7c0d3f1680b0fc7ffa08
kaiky de Moura <kaikymoura972@gmail.com>
2022-06-03 15:07:30 -0300

    Terminado =)
    .

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 create schema video_jc;
-
 use video_jc;
+
+/*SET FOREIGN_KEY_CHECKS=0;*/
 
 CREATE TABLE if not exists owner_details (  
     content_id varchar(24) NOT NULL,
@@ -55,24 +56,29 @@ values (' ', null);
 
 INSERT INTO channell (channellid, channelTitle, kind, content_id,link, publishedAt , description)
 VALUES   ('UCe1XGNDeEwAx5xaLGcNPEbQ', 'TV Jornal SBT', 'youtube#channel', ' ', 'https://www.youtube.com/channel/UCe1XGNDeEwAx5xaLGcNPEbQ', '2019-12-26', 
-          'No canal da TV Jornal SBT, você confere os principais vídeos de jornalismo e entretenimento da TV Jornal, emissora do Recife ...'),
-         ('UCOFMoDHG-vKXs-f5vcG7JaQ', 'TV Guararapes Oficial', 'youtube#channel', ' ', 'https://www.youtube.com/channel/UCOFMoDHG-vKXs-f5vcG7JaQ', '2015-03-30', 
-          'A TV Clube agora é TV Guararapes - Pernambuco em todas as telas. Sigam nossas redes sociais: Whatsapp: ...'),
-         ('jovempannews', 'Jovem Pan News', 'youtube#channel', ' ', 'https://www.youtube.com/c/jovempannews', '2011-12-01', 
-          'A maior rede de rádio do Brasil está também no Youtube com a presença de Thiago Uberreich, Rodrigo Constantino, Augusto Nunes e todo o time de comentaristas Jovem Pan.'),
-         ('UCQD_Fq8NZXbHz9LI8Fa1Q9A', 'TV Jornal Interior', 'youtube#channel', ' ', 'https://www.youtube.com/c/TVJornalInterior/featured', '2016-11-07', 
-          'Conteúdo produzido pela TV Jornal Interior/SBT Caruaru. Notícias de Pernambuco, jornalismo policial, futebol e entretenimento como Programa Cotidiano, Eu Fui Nesta Viagem, Programa Sobretudo e muito mais.');
+'No canal da TV Jornal SBT, você confere os principais vídeos de jornalismo e entretenimento da TV Jornal, emissora do Recife ...'),
+('UCOFMoDHG-vKXs-f5vcG7JaQ', 'TV Guararapes Oficial', 'youtube#channel', ' ', 'https://www.youtube.com/channel/UCOFMoDHG-vKXs-f5vcG7JaQ', '2015-03-30', 
+'A TV Clube agora é TV Guararapes - Pernambuco em todas as telas. Sigam nossas redes sociais: Whatsapp: ...'),
+('UCQD_Fq8NZXbHz9LI8Fa1Q9A', 'TV Jornal Interior', 'youtube#channel', ' ', 'https://www.youtube.com/c/TVJornalInterior/featured', '2016-11-07', 
+'Conteúdo produzido pela TV Jornal Interior/SBT Caruaru. Notícias de Pernambuco, jornalismo policial, futebol e entretenimento como Programa Cotidiano, Eu Fui Nesta Viagem, Programa Sobretudo e muito mais.'),
+('UCP391YRAjSOdM_bwievgaZA', 'Jovem Pan News', 'youtube#channel', ' ', 'https://www.youtube.com/c/jovempannews', '2011-12-01', 
+'A maior rede de rádio do Brasil está também no Youtube com a presença de Thiago Uberreich, Rodrigo Constantino, Augusto Nunes e todo o time de comentaristas Jovem Pan.'),
+('UCoa-D_VfMkFrCYodrOC9-mA', 'Band Jornalismo', 'youtube#channel', ' ','https://www.youtube.com/c/bandjornalismo/featured', '2016-08-08', 
+'Lives exclusivas para você participar e conversar com nossos especialistas e convidados. Veja ao vivo ou reveja na íntegra os nossos telejornais e programas. As melhores reportagens, os comentários do nosso time de jornalistas e especialistas e os principais conteúdos da tela da Band para você assistir quando e onde quiser.'),
+('UCa7utqA7qkZxm6RN2tM4ukg', 'TV Jornal Interior Ao vivo', 'youtube#channel', ' ', 'https://www.youtube.com/channel/UCa7utqA7qkZxm6RN2tM4ukg/about', 
+'2021-10-30', ' '),
+('Leonardo26PE','Leonardo26PE', 'youtube#channe',' ', 'https://www.youtube.com/user/Leonardo26PE/featured', '2008-07-02', ' ');
 
 INSERT INTO video (videoId, channellid, link, kind, title, publishedAt, description)
-VALUES  ('-mpFDHfb9pY', 'UCQD_Fq8NZXbHz9LI8Fa1Q9A', 'https://www.youtube.com/watch?v=-mpFDHfb9pY', 'youtube#video', 'Transmissão ao vivo de TV Jornal Interior', '2021-04-08', ' '),	 
-                ('4opM5Nt85yg', 'UCe1XGNDeEwAx5xaLGcNPEbQ', 'https://www.youtube.com/watch?v=4opM5Nt85yg', 'youtube#video', 'TV Jornal estreia novo estúdio e traz novidades na programação', '2021-09-13', 'A TV Jornal estreou nesta segunda-feira (13), o novo estúdio de Jornalismo da emissora trazendo novidades na programação.'),
-                ('6qLnAZ76AzI', 'UCOFMoDHG-vKXs-f5vcG7JaQ', 'https://www.youtube.com/watch?v=6qLnAZ76AzI', 'youtube#video', 'Tudo é Notícia -  AO VIVO', '2022-05-28', 'Tudo é Notícia - AO VIVO #tudoénotícia.'),
-                ('gTHSgcV4cPs', 'UCe1XGNDeEwAx5xaLGcNPEbQ', 'https://www.youtube.com/watch?v=gTHSgcV4cPs', 'youtube#video', '#AOVIVO | Pré-jogo entre Sport x Fortaleza pelo primeiro jogo da final da Copa do Nordeste 2022', '2022-04-01', 'Acompanhe todos os detalhes do pré-jogo entre Sport x Fortaleza, pelo primeiro jogo da final da Copa do Nordeste 2022, com ...'),
-                ('Ht5DL01JCLI', 'UCOFMoDHG-vKXs-f5vcG7JaQ', 'https://www.youtube.com/watch?v=Ht5DL01JCLI', 'youtube#video', 'Balanço Geral Manhã na íntegra - 25/05/2025', '2022-05-25', 'Whatsapp: encurtador.com.br/hlHIX Facebook: facebook.com/tvguararapesoficial Instagram: Instagram.com/tvguararapesoficial ...'),
-                ('TpphwOkx15M', 'UCe1XGNDeEwAx5xaLGcNPEbQ', 'https://www.youtube.com/watch?v=TpphwOkx15M', 'youtube#video', '#AOVIVO | Pré-jogo entre Sport x CRB pela Copa do Nordeste 2022', '2022-03-27', 'Acompanhe todos os detalhes do pré-jogo entre Sport x CRB, pela semifinal da Copa do Nordeste 2022, com Aroldo Costa.'),
-                ('twPEPnitz6U', 'UCP391YRAjSOdM_bwievgaZA', 'https://www.youtube.com/watch?v=twPEPnitz6U', 'youtube#video', 'JORNAL DA MANHÃ - 28/05/2022', '2022-05-28', 'Baixe o app Panflix: https://www.panflix.com.br/ Baixe o AppNews Jovem Pan na Google Play store https://bit.ly/2KRm8OJ Baixe o ...'),
-                ('B8zhql6aA08', 'UCoa-D_VfMkFrCYodrOC9-mA', 'https://www.youtube.com/watch?v=B8zhql6aA08', 'youtube#video', 'JORNAL DA MANHÃ - 28/05/2022', '2022-05-28', 'INSCREVA-SE NO CANAL E NÃO PERCA NENHUMA NOTÍCIA DO BRASIL E DO MUNDO. Siga as nossas redes sociais: ...'),
-                ('qyBsgDXDOKs', 'UCP4OAt3pxqIhh_DLy-SK20g', 'https://www.youtube.com/watch?v=qyBsgDXDOKs', 'youtube#video', 'Bronca Pesada com Cardinot (Trechos) - TV Jornal Digital - Recife/PE', '2011-07-09', 'A pedidos: Trechos do programa Bronca Pesada apresentado pelo Cardinot. Programa exibido em 7 de Julho de 2011. TV Jornal ...');
+VALUES ('-mpFDHfb9pY', 'UCQD_Fq8NZXbHz9LI8Fa1Q9A', 'https://www.youtube.com/watch?v=-mpFDHfb9pY', 'youtube#video', 'Transmissão ao vivo de TV Jornal Interior', '2021-04-08', ' '),	 
+('4opM5Nt85yg', 'UCe1XGNDeEwAx5xaLGcNPEbQ', 'https://www.youtube.com/watch?v=4opM5Nt85yg', 'youtube#video', 'TV Jornal estreia novo estúdio e traz novidades na programação', '2021-09-13', 'A TV Jornal estreou nesta segunda-feira (13), o novo estúdio de Jornalismo da emissora trazendo novidades na programação.'),
+('6qLnAZ76AzI', 'UCOFMoDHG-vKXs-f5vcG7JaQ', 'https://www.youtube.com/watch?v=6qLnAZ76AzI', 'youtube#video', 'Tudo é Notícia -  AO VIVO', '2022-05-28', 'Tudo é Notícia - AO VIVO #tudoénotícia.'),
+('gTHSgcV4cPs', 'UCe1XGNDeEwAx5xaLGcNPEbQ', 'https://www.youtube.com/watch?v=gTHSgcV4cPs', 'youtube#video', '#AOVIVO | Pré-jogo entre Sport x Fortaleza pelo primeiro jogo da final da Copa do Nordeste 2022', '2022-04-01', 'Acompanhe todos os detalhes do pré-jogo entre Sport x Fortaleza, pelo primeiro jogo da final da Copa do Nordeste 2022, com ...'),
+('Ht5DL01JCLI', 'UCOFMoDHG-vKXs-f5vcG7JaQ', 'https://www.youtube.com/watch?v=Ht5DL01JCLI', 'youtube#video', 'Balanço Geral Manhã na íntegra - 25/05/2025', '2022-05-25', 'Whatsapp: encurtador.com.br/hlHIX Facebook: facebook.com/tvguararapesoficial Instagram: Instagram.com/tvguararapesoficial ...'),
+('TpphwOkx15M', 'UCe1XGNDeEwAx5xaLGcNPEbQ', 'https://www.youtube.com/watch?v=TpphwOkx15M', 'youtube#video', '#AOVIVO | Pré-jogo entre Sport x CRB pela Copa do Nordeste 2022', '2022-03-27', 'Acompanhe todos os detalhes do pré-jogo entre Sport x CRB, pela semifinal da Copa do Nordeste 2022, com Aroldo Costa.'),
+('twPEPnitz6U', 'UCP391YRAjSOdM_bwievgaZA', 'https://www.youtube.com/watch?v=twPEPnitz6U', 'youtube#video', 'JORNAL DA MANHÃ - 28/05/2022', '2022-05-28', 'Baixe o app Panflix: https://www.panflix.com.br/ Baixe o AppNews Jovem Pan na Google Play store https://bit.ly/2KRm8OJ Baixe o ...'),
+('nAP-Vjz-dtU', 'UCa7utqA7qkZxm6RN2tM4ukg', 'https://www.youtube.com/watch?v=nAP-Vjz-dtU', 'youtube#video', 'SEM MEIAS PALAVRAS | 03.06.2022', '2022-06-03', ' '),
+('qyBsgDXDOKs', 'Leonardo26PE','https://www.youtube.com/watch?v=qyBsgDXDOKs', 'youtube#video', 'Bronca Pesada com Cardinot (Trechos) - TV Jornal Digital - Recife/PE', '2011-07-09', 'A pedidos: Trechos do programa Bronca Pesada apresentado pelo Cardinot - Programa exibido em 7 de Julho de 2011.');
 
 INSERT INTO video_statistics (viewCount, likeCount, dislikeCount, commentCount, videoId)
 VALUES  ('16.374', '362', ' ' , '29', "-mpFDHfb9pY" ),
@@ -82,17 +88,14 @@ VALUES  ('16.374', '362', ' ' , '29', "-mpFDHfb9pY" ),
         ('30.114', '978', ' ' , '20', "Ht5DL01JCLI" ),
 	('24.512', '582', ' ' , '8', "TpphwOkx15M" ),
 	('21.607', '422', ' ' , '5', "twPEPnitz6U" ),
-        ('134.091', ' ', ' ' , '39', "B8zhql6aA08" ),
 	('1.257.440', '8.000', ' ' , '327', "qyBsgDXDOKs" );
         
 INSERT INTO channel_statistics (viewCount, channellid, subscriberCount, videoCount)
 VALUES  ('17.048.470', "UCe1XGNDeEwAx5xaLGcNPEbQ", '110.000' , '6.849'),
 	('98.655.056', "UCOFMoDHG-vKXs-f5vcG7JaQ", '456.000' , '11.334'),
 	('2.001.756.622', "UCP391YRAjSOdM_bwievgaZA", '5.0400.00' , '80.958'),
-	('20.162.983', "UCQD_Fq8NZXbHz9LI8Fa1Q9A", '103.000' , '4.469');
-        ('0', "UCAQN1P1htBCZKvj2vcFCcmw", '82.2000' , '0'),
+	('20.162.983', "UCQD_Fq8NZXbHz9LI8Fa1Q9A", '103.000' , '4.469'),
         ('296.392', "UCa7utqA7qkZxm6RN2tM4ukg", '5.2000' , '393'),
-        (' ', ' ', ' ' , ' '),
         ('1.548.985.663', "UCoa-D_VfMkFrCYodrOC9-mA", '3.75000' , '79.690'),
         ('27.493.627', "Leonardo26PE", '58.2000' , '599');
     
@@ -110,5 +113,3 @@ WHERE video_statistics.videoId = video.videoId
 AND video.channellid = channell.channellid
 AND channell.channellid = channel_statistics.channellid
 ORDER BY video_statistics.viewCount DESC;
-ORDER BY video_statistics.viewCount DESC;
-
